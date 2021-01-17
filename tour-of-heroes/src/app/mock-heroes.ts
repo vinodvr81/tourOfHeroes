25a1c3c2fb303e1640ca8078bb18060a9dfc2587
vinodvr81 <vinod.vukkalam@gmail.com>
2021-01-17 07:58:46 +0530

    Added list of Heros

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Chiranjeevi' },
+  { id: 2, name: 'Balakrishna' },
+  { id: 3, name: 'Nagarjuna' },
+  { id: 4, name: 'Venkatesh' },
+  { id: 5, name: 'Nani' },
+  { id: 6, name: 'Vijay' },
+  { id: 7, name: 'Arjun' },
+  { id: 8, name: 'Vishvak' },
+  { id: 9, name: 'Naresh' },
+  { id: 10, name: 'Ravi Teja' },
+  { id: 11, name: 'Ramesh' }
+];
